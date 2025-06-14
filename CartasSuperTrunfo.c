92567752f4cc6d0105fdd2bf9d9f011759afea85
Marcelo Nascimento <105380422+marcelo25sp@users.noreply.github.com>
2025-06-14 20:49:35 +0000

    Escrevendo as linhas de código da Carta 1

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -14,5 +14,23 @@ int main() {
     int habitantes1, habitantes2, pontosTuristicos1, pontosTuristicos2;
     float pib1, pib2, area1, area2; 
 
+    // entrada dos dados da carta 1    
+    printf("Entre com os dados da Carta 1:\n");
+    printf("\n");
+    printf("Estado: ");
+    scanf("%c", &estado1);
+    printf("Código: ");
+    scanf("%s", &codigo1);
+    printf("Nome da cidade: ");
+    scanf("%s", &nomeCidade1);
+    printf("População: ");
+    scanf("%d", &habitantes1);
+    printf("Área: ");
+    scanf("%f", &area1);
+    printf("PIB: ");
+    scanf("%f", &pib1);
+    printf("Número de pontos turísticos: ");
+    scanf("%d", &pontosTuristicos1);
+
     return 0;
 }
