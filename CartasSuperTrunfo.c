61ed2e5f486bf737340dda7144fd646337710497
Marcelo Nascimento <105380422+marcelo25sp@users.noreply.github.com>
2025-06-15 18:45:52 +0000

    Implementando os cálculos e saída da densidade populacional e renda per capita das cidades

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -12,7 +12,7 @@ int main() {
     char codigo1[8], codigo2[8];
     char nomeCidade1[30],nomeCidade2[30];
     int habitantes1, habitantes2, pontosTuristicos1, pontosTuristicos2;
-    float pib1, pib2, area1, area2; 
+    float pib1, pib2, area1, area2, densidade1, densidade2, pibPerCapita1, pibPerCapita2; 
 
     // entrada dos dados da carta 1    
     printf("Entre com os dados da Carta 1:\n");
@@ -32,17 +32,21 @@ int main() {
     printf("Número de pontos turísticos: ");
     scanf("%d", &pontosTuristicos1);
 
+    densidade1 = (float) habitantes1/area1; // calculando a densidade populacional  da Carta 1 utilizando o casting
+    pibPerCapita1 = (float) pib1/habitantes1; // calculando o pib per capita da Carta 1 utilizando o casting
+
     // exibindo dados da Carta 1
     printf("\n");
-    printf("Carta 1\n");
-    printf("\n");
+    printf("Carta 1:\n");
     printf("Estado: %c\n", estado1);
     printf("Código: %s\n", codigo1);
     printf("Nome da Cidade: %s\n", nomeCidade1);
     printf("População: %d\n", habitantes1);
-    printf("Área: %f\n", area1);
-    printf("PIB: %f\n", pib1);
+    printf("Área: %.2f km²\n", area1);
+    printf("PIB: %.2f bilhões de reais\n", pib1);
     printf("Pontos Turísticos: %d\n", pontosTuristicos1);
+    printf("Densidade Populacional: %.2f hab/km²\n", densidade1);
+    printf("PIB per Capita: %.2f reais\n", pibPerCapita1);
 
     // entrada dos dados da Carta 2
     printf("\n");
@@ -63,17 +67,21 @@ int main() {
     printf("Número de pontos turísticos: ");
     scanf("%d", &pontosTuristicos2);
 
-        // exibindo dados da Carta 2
+    densidade2 = (float) habitantes2/area2; // calculando a densidade populacional da Carta 2 utilizando o casting
+    pibPerCapita2 = (float) pib2/habitantes2; // calculando o pib per capita da Carta 2 utilizando o casting
+
+    // exibindo dados da Carta 2
     printf("\n");
-    printf("Carta 2\n");
-    printf("\n");
+    printf("Carta 2:\n");
     printf("Estado: %c\n", estado2);
     printf("Código: %s\n", codigo2);
     printf("Nome da Cidade: %s\n", nomeCidade2);
     printf("População: %d\n", habitantes2);
-    printf("Área: %f\n", area2);
-    printf("PIB: %f\n", pib2);
+    printf("Área: %.2f km²\n", area2);
+    printf("PIB: %.2f bilhões de reais\n", pib2);
     printf("Pontos Turísticos: %d\n", pontosTuristicos2);
- 
+    printf("Densidade Populacional: %.2f hab/km²\n", densidade2);
+    printf("PIB per Capita: %.2f reais\n", pibPerCapita2);
+
     return 0;
 }
